279bd7264372a6d3b70543e401112ad4da20eaf8
Greg Mercer (GBear) <gmercer@stanford.edu>
2012-09-11 11:40:44 -0700

    more testing

--- build.sh
+++ build.sh
@@ -14,11 +14,6 @@ else
     echo "gsb-distro directory exists"
 fi
 
-echo "outa here"
-exit -1
-
-echo "what"
-
 if [[ ! -e revamp ]]; then
     git clone -b $server revamp@svn-634.devcloud.hosting.acquia.com:revamp.git
     cd revamp
@@ -35,27 +30,33 @@ if
 cd gsb-distro
 git show-branch $branch
 if [[ $? != 0 ]]; then
+    cd ..
     git clone -b $branch https://github.com/gsbitse/gsb-distro.git
+    cd gsb-distro
     git show-branch $branch
     if [[ $? != 0 ]]; then
         echo "failed to clone gsb-distro branch = $branch"
         exit -1
     fi    
+    cd ..
     echo "gsb-distro branch = $branch was cloned" 
 fi
 
-cd ..
-
-cd revamp 
+cd revamp
 git show-branch $server
 if [[ $? != 0 ]]; then
+    cd ..
     git clone -b $server revamp@svn-634.devcloud.hosting.acquia.com:revamp.git
+    cd revamp
     git show-branch $server
     if [[ $? != 0 ]]; then
         echo "failed to clone revamp branch = $server"
         exit -1
     fi
+    cd ..
     echo "revamp branch = $server was cloned"
 fi
+
+
 
 
