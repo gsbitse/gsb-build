c905aa5a3a11eb9f11b460a23b7a094d4d5f456e
Greg Mercer (GBear) <gmercer@stanford.edu>
2012-09-11 10:21:43 -0700

    adding directory checking to the script

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-echo "this is a test branch = $branch, server = $server"
+if [[ ! -e gsb-build ]]; then
+    echo "directory gsb-build does not exist\n"
+    exit -1
+fi
+
+if [[ ! -e gsb_distro ]]; then
+    git clone https://github.com/gsbitse/gsb-distro.git
+    echo "gsb-distro directory created\n"
+elif then
+    echo "gsb-distro directory exists\n"
+fi
 
 
