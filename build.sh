ecd9736bb642168dcee9a880577c18b9570df288
Greg Mercer <gmercer@yahoo.com>
2012-09-12 11:22:00 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -37,10 +37,72 @@ cd ${workspace_dir}/gsb-distro
 ret_code=$(git ls-remote $distro_url $branch | wc -l | tr -d ' ')
 
 if [[ $ret_code == 1 ]]; then
-    git checkout $server
+    git checkout $branch
     echo "gsb-distro checkout branch = $branch"
 else
     echo "gsb-distro branch = $branch not found"
     exit -1
 fi
+
+############################################
+# check if the revamp directory exists
+# if it doesn't clone it
+
+cd $workspace_dir
+
+if [ ! -d revamp ]; then
+    git clone -b $server $revamp_url 
+    if [ ! -d revamp ]; then
+       echo "revamp cloned failed for branch = $server"
+       exit -1
+    fi    
+    echo "revamp directory created"
+else
+    echo "revamp directory exists"
+fi
+
+############################################
+# check if the revamp branch exists
+# if it does then check it out
+
+cd ${workspace_dir}/revamp
+
+ret_code=$(git ls-remote $revamp_url $server | wc -l | tr -d ' ')
+
+if [[ $ret_code == 1 ]]; then
+    git checkout $server
+    echo "revamp checkout branch = $server"
+else
+    echo "revamp branch = $server not found"
+    exit -1
+fi
+
+############################################
+# change to the revamp directory
+# and then run the drush make
+
+cd ${workspace_dir}/revamp
+
+rm -rf docroot
+php library/drush/drush.php make ../gsb-distro/gsb-public-distro.make docroot
+
+############################################
+# add back in the symlink for the files
+# directory
+
+# ln -s docroot/sites/default/files /mnt/?????
+
+############################################
+# add the changes up to acquia
+
+git add .
+git commit -am "build from cloudbees - project: revamp  branch: $branch server: $server"
+git push origin $server
+
+############################################
+# end of build script 
+#
+
+
+
 
