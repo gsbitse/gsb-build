6f869a19fab1ef3a7a57a2150012ab4d2ea751da
Greg Mercer <gmercer@stanford.edu>
2013-03-20 15:35:58 -0700

    Update build_boo.php

--- build_boo.php
+++ build_boo.php
@@ -1,4 +1,8 @@
 <?php
 exec('git ls-remote -h https://github.com/gsbitse/gsb-distro.git', $output);
-print('branches='.preg_replace('/[a-z0-9]*\trefs\/heads\//','',implode(',', $output)));
+$branches_out = 'branches=' . preg_replace('/[a-z0-9]*\trefs\/heads\//','',implode(',', $output));
+$myFile = "zz.txt";
+$fh = fopen($myFile, 'w');
+fwrite($fh, $branches_out);
+fclose($fh);
 ?>
