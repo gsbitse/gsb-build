775065f81562a4a2d18941f500257fce14105afa
Greg Mercer <gmercer@stanford.edu>
2013-08-23 13:59:24 -0700

    Update build_acquia_public_prod.sh

--- build_acquia_public_prod.sh
+++ build_acquia_public_prod.sh
@@ -6,12 +6,6 @@
 distro_url="https://github.com/gsbitse/gsb-distro.git"
 publicsite_url="gsbpublic@svn-3224.prod.hosting.acquia.com:gsbpublic.git"
 
-publicsite_dev_ssh="gsbpublic@staging-1530.prod.hosting.acquia.com"
-publicsite_dev2_ssh="gsbpublic@staging-1530.prod.hosting.acquia.com"
-publicsite_stage_ssh="gsbpublic@ded-2036.prod.hosting.acquia.com"
-publicsite_stage2_ssh="gsbpublic@ded-2036.prod.hosting.acquia.com"
-publicsite_sandbox_ssh="gsbpublic@staging-1530.prod.hosting.acquia.com"
-publicsite_loadtest_ssh="gsbpublic@ded-1505.prod.hosting.acquia.com"
 publicsite_prod_ssh="gsbpublic@ded-1528.prod.hosting.acquia.com"
 
 ############################################
@@ -22,36 +16,6 @@ workspace_dir=$PWD
 ############################################
 # set the public site ssh we will be using
 # based on which server we are building on
-
-if test $server = "dev"
-then
-  publicsite_ssh=$publicsite_dev_ssh
-fi
-
-if test $server = "dev2"
-then
-  publicsite_ssh=$publicsite_dev2_ssh
-fi
-
-if test $server = "stage"
-then
-  publicsite_ssh=$publicsite_stage_ssh
-fi
-
-if test $server = "stage2"
-then
-  publicsite_ssh=$publicsite_stage2_ssh
-fi
-
-if test $server = "sandbox"
-then
-  publicsite_ssh=$publicsite_sandbox_ssh
-fi
-
-if test $server = "loadtest"
-then
-  publicsite_ssh=$publicsite_loadtest_ssh
-fi
 
 if test $server = "prod"
 then
@@ -64,9 +28,9 @@ fi
 
 cd ${workspace_dir}/gsb-distro
 
-ret_code=$(git ls-remote $distro_url $branch | wc -l | tr -d ' ')
+ret_code=$(git ls-remote $distro_url $tag | wc -l | tr -d ' ')
 if [[ $ret_code != 1 ]]; then
-    echo "gsb-distro branch = $branch not found"
+    echo "gsb-distro tag = $tag not found"
     exit -1
 else
     git pull 
@@ -80,7 +44,7 @@ cd ${workspace_dir}/gsbpublic
 
 ret_code=$(git ls-remote $publicsite_url $server | wc -l | tr -d ' ')
 if [[ $ret_code != 1 ]]; then
-    echo "gsbpublic branch = $server not found"
+    echo "gsbpublic server = $server not found"
     exit -1
 else
     git pull 
@@ -93,9 +57,9 @@ fi
 cd $workspace_dir
 
 if [ ! -d gsb-distro ]; then
-    git clone -b $branch $distro_url
+    git clone -b $tag $distro_url
     if [ ! -d gsb-distro ]; then
-       echo "gsb-distro cloned failed for branch = $branch"
+       echo "gsb-distro cloned failed for tag = $tag"
        exit -1
     fi
     echo "gsb-distro directory created"
@@ -104,11 +68,11 @@ else
 fi
 
 ############################################
-# checkout the gsb-distro branch
+# checkout the gsb-distro tag
 # 
 
 cd ${workspace_dir}/gsb-distro
-git checkout $branch
+git checkout $tag
 
 ############################################
 # check if the gsbpublic directory exists
@@ -119,7 +83,7 @@ cd $workspace_dir
 if [ ! -d gsbpublic ]; then
     git clone -b $server $publicsite_url 
     if [ ! -d gsbpublic ]; then
-       echo "gsbpublic cloned failed for branch = $server"
+       echo "gsbpublic cloned failed for server = $server"
        exit -1
     fi    
     echo "gsbpublic directory created"
@@ -128,7 +92,7 @@ else
 fi
 
 ############################################
-# checkout the gsbpublic branch
+# checkout the gsbpublic server
 # 
 
 cd ${workspace_dir}/gsbpublic
