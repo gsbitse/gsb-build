774a40c5f9193efc8054c57b551befc9362e57b1
Adam Moore <redndahead@gmail.com>
2014-08-28 15:05:54 -0700

    Add a latest branches builder.

---
+++ build_branches_selector_latest.php
@@ -0,0 +1,22 @@
+<?php
+exec('git ls-remote -h https://github.com/gsbitse/gsb-distro.git', $output);
+
+$versions = array();
+foreach ($output as $branch) {
+  if (stristr($branch, 'release-')) {
+    $branch_array = explode('release-', $branch);
+    $versions[] = $branch_array[1];
+  }
+}
+
+usort($versions, 'version_compare');
+$versions = array_slice($versions, -5);
+foreach ($versions as $index => $version) {
+  $versions[$index] = 'release-' . $version;
+}
+$branches_out = 'branches=' . implode(',', $versions);
+$file_out = "branches-latest.txt";
+$fh = fopen($file_out, 'w');
+fwrite($fh, $branches_out);
+fclose($fh);
+?>
