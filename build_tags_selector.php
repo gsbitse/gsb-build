3a30d59ee624797444be583027e05dc23f835946
Greg Mercer <gmercer@stanford.edu>
2015-11-17 16:07:17 -0800

    Limit the list of tags to just the last 5.

--- build_tags_selector.php
+++ build_tags_selector.php
@@ -1,5 +1,6 @@
 <?php
 exec('git ls-remote --tags https://github.com/gsb-public/gsb-public-distro.git', $output);
+$output = array_slice($output, -5);
 $tags_out = 'tags=' . preg_replace('/[a-z0-9]*\trefs\/tags\//','',implode(',', $output));
 $file_out = "tags.txt";
 $fh = fopen($file_out, 'w');
